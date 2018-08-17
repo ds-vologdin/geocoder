82b736a9aa3307a3f9bca9be7ceff6681718abb2
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-08-17 15:07:30 +0300

    работа с csv

--- geocoder.py
+++ geocoder.py
@@ -1,9 +1,10 @@
 import argparse
 import logging
+import csv
 
 from geocode_yandex import fetch_geocode_yandex_json
 from geocode_yandex import parse_geocode_yandex_to_geoobjects
-from logger import convert_str_to_logging_level
+from logger import convert_str_to_logging_level, logger
 
 
 def parse_argv():
@@ -45,11 +46,39 @@ def output_result(geoobjects):
         print('{};{}'.format(geoobject['point'], geoobject['address']))
 
 
-def output_geocode_from_address(raw_addres):
-    address = format_address(raw_addres)
+def get_geoobdjects_for_address(raw_address):
+    address = format_address(raw_address)
     geocode = fetch_geocode_yandex_json(address)
-    geoobdjects = parse_geocode_yandex_to_geoobjects(geocode)
+    return parse_geocode_yandex_to_geoobjects(geocode)
+
+
+def output_geocode_from_address(raw_address):
+    geoobdjects = get_geoobdjects_for_address(raw_address)
     output_result(geoobdjects)
+
+
+def write_geoobjects_to_csv_writer(csv_writer, geoobdjects, row):
+    for geoobdject in geoobdjects:
+        output_row = [geoobdject['point'], geoobdject['address']]
+        output_row += row
+        csv_writer.writerow(output_row)
+
+
+def _set_geocode_to_csv(csv_file):
+    with open(csv_file, newline='') as input_file, open('output_'+csv_file, 'w', newline='') as output_file:
+        output_writer = csv.writer(output_file)
+        for row in csv.reader(input_file):
+            raw_address = row[0]
+            geoobdjects = get_geoobdjects_for_address(raw_address)
+            write_geoobjects_to_csv_writer(output_writer, geoobdjects, row)
+
+
+def set_geocode_to_csv(csv_file):
+    try:
+        _set_geocode_to_csv(csv_file)
+    except IOError as e:
+        logger.error(e)
+        raise e
 
 
 def main():
@@ -62,6 +91,9 @@ def main():
     )
     if args.address:
         output_geocode_from_address(args.address)
+
+    if args.csv_file:
+        set_geocode_to_csv(args.csv_file)
 
 
 if __name__ == '__main__':
